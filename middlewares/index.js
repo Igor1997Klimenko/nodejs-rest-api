66719fbaec2020672a529f65be51563c3ed4ca9d
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2022-09-16 20:01:23 +0300

    finish hw05

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validationContacts = require('./validationContacts');
 const isValidId = require('./isValidId');
 const authenticate = require('./authenticate');
+const upload = require('./upload');
 
 module.exports = {
     validationContacts,
     isValidId,
-    authenticate
+    authenticate,
+    upload,
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,7 +6,7 @@ const {ctrlWrapper} = require('../../helpers');
 
 const ctrl = require('../../controllers/auth')
 
-const {validationContacts, authenticate} = require('../../middlewares/');
+const {validationContacts, authenticate, upload} = require('../../middlewares/');
 
 const {schemas} = require('../../models/user');
 
@@ -15,5 +15,7 @@ router.post('/signup', validationContacts(schemas.signupSchema), ctrlWrapper(ctr
 router.post('/login', validationContacts(schemas.loginSchema), ctrlWrapper(ctrl.login));
 
 router.get('/logout', authenticate, ctrlWrapper(ctrl.logout));
+
+router.patch('/avatars', authenticate, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar));
 
 module.exports = router;
